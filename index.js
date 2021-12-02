bc51b2051efa3933f11acb65409c1db15d00dd10
Emily <emilybribragg@gmail.com>
2021-12-01 19:03:06 -0500

    commit tree 8764086ba8c993feacfb310ce314692c2c013a3a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// returns undefined if the record has no win objects
 
+function superbowlWin(record){
+   const win = record.find(object => object.result === "W")
+   if (win){
+    return win.year
+}   else {
+    return undefined
+}
+}
+
+
+// if we find a win:
+    //   return the win's year
+    // else 
+    //   return win (undefined)
